511673cf0d99ae2ec8dcc2bc3d4578daa8680eaa
Kevin J Walters <kjw.github@gmail.com>
2019-04-19 00:57:02 +0100

    Increasing amplitude range a little to +/- 1.3. Typo in fundover() (was only working because of function defined in R session). fundoversum() now splits lines as bigger font causes overflow. Adding graph$frequencyfunc for future use. Changing sawtooth phase/etc to make it look like classic representation. Increased harmonics to 10 in total after checking animated gif size (about 4MB). Doubling size of colour "sparkle" to 6 positions. Degrees labelled and now every 90 to reduce clutter. Cleaning up power spectrum breaks and using NA rather than 0 for negative values. Longer repetition on final frame. General labelling.

--- controller-synth/waveforms/graphing/waveforms.R
+++ controller-synth/waveforms/graphing/waveforms.R
@@ -5,46 +5,42 @@ library(grid)   ### for textGrob
 library(data.table)
 library(english)
 
+### Imagemagick creation of looping animated gif
+### convert -delay 25 -resize 480x360 +repage $(ls waveform-construction-sawtooth-i00*) -loop 0 ../sawtooth.test.2.gif
+
 filebase <- "waveform-construction-"
 
 samplelen <- 512
+# small number gives smoother, longer animated wave addition
 chunksize <- 64
-waverange <- 1.2
-
-### TODO - make graphs look pretty
-### get magnitude right
-### do i want to swtich to -1.2 to 1.2 for overshoots
-### get x axis right
-### discard second half of FFT
-### look at previous graphs for inspiration as they were pretty clean
-### colour? could add could during graph anim to show it being "drawn" on final point
-### some labels?
-### label as fundamental, first harmonic, second harmonic?
-### maybe the answer is not to repeat final image but do NON looping animated gif
+waverange <- 1.3
 
 ### geom_step looks useful for any future reconstruction graphs
 ### also rep(a,each=18/length(a)) can resample array without interpolation
 
 # Create at 1600:1200 (4:3 ratio) then scale down later for animated gif (480 x 360 ?)
 gdpi <- 100
-gheight <- 16
-gwidth <- 12
+gheight <- 12
+gwidth <- 16
 
 # return fundamental or overtone in round brackets
 fundover <- function(i) {
-  ifelse(i==1, "(fundamental)", paste("(",ordinal(i-1)," overtone)",sep="")
+  ifelse(i==1, "(fundamental)", paste("(",ordinal(i-1)," overtone)",sep=""))
 }
 
 # return fundamental and overtone(s) summary
+# need linebreak for big fonts
 fundoversum <- function(i) {
   if(i==1) {
     "(fundamental only)"
-  } else if (i == 2) {
-    "(fundamental + 1 overtone)"
+  } else if(i == 2) {
+    "(fundamental\n+ 1 overtone)"
   } else { 
-    paste("(fundamental + ",i-1," overtones)",sep="")
+    paste("(fundamental +\n",i-1," overtones)",sep="")
   }
 }
+
+proper <- function(s) sub("(.)", ("\\U\\1"), tolower(s), pe=TRUE)
 
 ### functions are not a vector type so must be stored in a list
 ### I() needs to be used to stop automatic attempt to convert to vector
@@ -58,9 +54,11 @@ fundoversum <- function(i) {
 ### data.table can hold lists
 graphs <- data.table(name=c("sawtooth",
                             "square"),
-                     rep=c(5, 5),
-                     func=list(function (n, x) { 1/pi * (-1)^n * 1/n * sin(n * x) },
-                               function (n, x) { 4/pi * 1/(n*2-1) * sin((n*2-1)*x) } )
+                     rep=c(10, 10),
+                     wavefunc=list(function (n, x) { 2/pi * (-1)^(n-1) * 1/n * sin(n * (x-pi)) },
+                                   function (n, x) { 4/pi * 1/(n*2-1) * sin((n*2-1)*x) } ),
+                     frequencyfunc=list(function (n) { n },
+                                        function (n) { n*2-1 } )
                     )
 
 # possibly a better way to do this
@@ -69,7 +67,7 @@ blank <- textGrob("")
 for(gi in 1:nrow(graphs)) {
     name <- graphs$name[[gi]]
     rep  <- graphs$rep[[gi]]
-    func <- graphs$func[[gi]]
+    wavefunc <- graphs$wavefunc[[gi]]
     
     ### NA will stop a line being plotted
     wave <- rep(NA, samplelen)
@@ -78,7 +76,7 @@ for(gi in 1:nrow(graphs)) {
         from <- seq(1, samplelen, chunksize)
         to <- tail(c(from-1, samplelen), -1)  # tail -1 removing first element
         
-        wave_add <- func(i, 0:(samplelen-1) / samplelen * 2 * pi)
+        wave_add <- wavefunc(i, 0:(samplelen-1) / samplelen * 2 * pi)
         
         frame <- 1
         for(ci in seq(1, length(from))) {
@@ -95,32 +93,36 @@ for(gi in 1:nrow(graphs)) {
                               sparkle=rep(NA, 512))
             lastxdeg <- lastx*360/512
 
-            ### assumes there are at least three points per chunk
-            dat$sparkle[(lastx-2):lastx] <- c("c1", "c2", "c3")
+            ### assumes there are at least SIX points per chunk
+            dat$sparkle[(lastx-5):lastx] <- paste0("c",1:6)
                               ### TODO change Y of line
             ptl <- ggplot(dat, aes(x=xdeg, y=wave)) +
                    theme_light(base_size=28) +
+                   labs(x="degrees", y="amplitude") +
                    geom_point() +
-                   scale_x_continuous(breaks=seq(0, 360, by=45)) +
+                   scale_x_continuous(breaks=seq(0, 360, by=90)) +
                    geom_segment(aes(x=lastxdeg, y=-waverange, xend=lastxdeg, yend=wave),
                                 data=dat[lastx,],
                                 color="darkgray", linetype="dashed", size=2) +
-                   geom_point(shape=8, size=4, na.rm = TRUE, aes(color=sparkle)) +
-                   scale_colour_manual(values=c("yellow","orange","red"), guide=FALSE) +
+                   geom_point(shape=8, size=5, na.rm = TRUE, aes(color=sparkle)) +
+                   scale_colour_manual(values=rep(c("yellow","orange","red"), each=2),
+                                       guide=FALSE) +
                    coord_cartesian(ylim = c(-waverange, waverange))
             ptr <- blank
             pbl <- ggplot(dat, aes(x=xdeg, y=wave_add)) +
                    theme_light(base_size=28) +
-                   geom_point() + 
-                   scale_x_continuous(breaks=seq(0, 360, by=45)) +
+                   labs(name="Adding harmonic wave", x="degrees", y="amplitude") +
+                   geom_point() +
+                   scale_x_continuous(breaks=seq(0, 360, by=90)) +
                    geom_segment(aes(x=lastxdeg, y=wave_add, xend=lastxdeg, yend=waverange),
                                 data=dat[lastx,],
                                 color="darkgray", linetype="dashed", size=2) +
                    coord_cartesian(ylim = c(-waverange, waverange))
-            pbr <- textGrob(paste(name, "wave\n",
+            pbr <- textGrob(paste(proper(name), "wave\n",
                                   ordinal(i), "harmonic\n",
-                                  fundover(i)))
-                            
+                                  fundover(i)),
+                            gp = gpar(fontsize=60))
+
             ptable <- arrangeGrob(ptl, ptr, pbl, pbr, ncol=2)
 
             # show on screen
@@ -129,20 +131,18 @@ for(gi in 1:nrow(graphs)) {
             ### Save as 1600x1200 (4:3) png
             ### TODO ADD ANOTHER SEQUENCE NUMBER IN HERE 0 PAD
             ggsave(paste(filebase, name,
-                         sprintf("i%03df%03d", i, frame), ".png", sep=""),
+                         sprintf("-i%03df%03d", i, frame), ".png", sep=""),
                    ptable,
                    dpi=gdpi, height=gheight, width=gwidth, units="in")
             frame <- frame + 1
         }
-        
-        wave <- wave + wave_add
-        
+
         # take the first half of magnitude fft, rest is the same thing
         fft_magn_wave <- abs(fft(wave))
         ### R is ok with log10(0), easier to plot with geom_col if positive
         power_spectrum_db <- log10(fft_magn_wave / max(fft_magn_wave)) * 20
-        power_spectrum_db_pos60 <= ifelse(power_spectrum_db > -60,
-                                          power_spectrum_db + 60, 0)
+        power_spectrum_db_pos60 <- ifelse(power_spectrum_db > -60,
+                                          power_spectrum_db + 60, NA)
 
         dat <- data.frame(x=seq(0, 511),
                           xdeg=seq(0, 511) * 360/512,
@@ -154,21 +154,30 @@ for(gi in 1:nrow(graphs)) {
 
         ptl <- ggplot(dat, aes(x=xdeg, y=wave)) +
                theme_light(base_size=28) +
+               labs(x="degrees", y="amplitude") +
                geom_point() +
-               scale_x_continuous(breaks=seq(0, 360, by=45)) +
+               scale_x_continuous(breaks=seq(0, 360, by=90)) +
                coord_cartesian(ylim = c(-waverange, waverange))
         ptr <- ggplot(dat, aes(x=factor(x), y=power_spectrum_db_pos60)) +
                theme_light(base_size=28) +
-               geom_point() +
+               theme(panel.grid.major.x = element_blank(),
+                     panel.grid.minor.x = element_blank(),
+                     axis.text.x=element_text(hjust=0.5, vjust=0.55, angle=90, size=16)) +
+               labs("Power spectrum", x="frequency/Hz", y="power/dB") +
+               geom_col(color="darkgreen", fill="darkgreen", width=0.7) +
+               scale_x_discrete(breaks=1:(20-1)) + 
+               scale_y_continuous(breaks=seq(0, 60, by=10)) +
                coord_cartesian(xlim=c(1, 20), ylim = c(0, 60))
         pbl <- ggplot(dat, aes(x=xdeg, y=wave_add)) +
                theme_light(base_size=28) +
+               labs(name="Last harmonic wave", x="degrees", y="amplitude") +
                geom_point() +
-               scale_x_continuous(breaks=seq(0, 360, by=45)) +
+               scale_x_continuous(breaks=seq(0, 360, by=90)) +
                coord_cartesian(ylim = c(-waverange, waverange))
-        pbr <- textGrob(paste(name, "wave\n",
+        pbr <- textGrob(paste(proper(name), "wave\n",
                               i, "harmonics\n",
-                              fundoversum(i)))
+                              fundoversum(i)),
+                        gp = gpar(fontsize=60))
 
         ptable <- arrangeGrob(ptl, ptr, pbl, pbr, ncol=2)
 
@@ -178,9 +187,10 @@ for(gi in 1:nrow(graphs)) {
         ### Save as 1600x1200 (4:3) png
         ### Generate multiple plots to duplicate frame to dwell on image longer
         ### in animation (could just run copy command here?)
-        for(rependframe in 1:4) {
+        duplicates <- ifelse(i==rep, 20, 10)  # do 20 on final image
+        for(rependframe in 1:duplicates) {
             ggsave(paste(filebase, name,
-                         sprintf("i%03df%03d", i, frame), ".png", sep=""),
+                         sprintf("-i%03df%03d", i, frame), ".png", sep=""),
                    ptable,
                    dpi=gdpi, height=gheight, width=gwidth, units="in")
             frame <- frame + 1
